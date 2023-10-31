70b9bc7e7544f4bc3426279c8c2958d01045e128
JoeHn01 <yousef.hanafi2001@gmail.com>
2023-10-31 15:45:06 -0600

    First App Commit - Initialization

---
+++ store/lib/store_web/controllers/page_controller.ex
@@ -0,0 +1,25 @@
+defmodule StoreWeb.PageController do
+  use StoreWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+
+  def menu(conn, _params) do
+    render(conn, :menu, layout: false)
+  end
+
+  def about(conn, _params) do
+    render(conn, :about, layout: false)
+  end
+
+  def products(conn, _params) do
+    render(conn, :products, layout: false)
+  end
+
+  def contact(conn, _params) do
+    render(conn, :contact, layout: false)
+  end
+end
